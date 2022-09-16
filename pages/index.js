489d90645a66fd680e39461c77641f42d3824344
levblanc <levblanc@gmail.com>
2022-09-16 16:25:07 +0800

    Add `Header` with `@web3uikit`

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
-// import Header from '../components/Header';
-import MannualHeader from '../components/MannualHeader';
+import Header from '../components/Header';
+// import MannualHeader from '../components/MannualHeader';
 
 export default function Home() {
   return (
@@ -12,8 +12,8 @@ export default function Home() {
         <meta name="description" content="Smart contract lottery" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <MannualHeader />
-      {/* <Header /> */}
+      {/* <MannualHeader /> */}
+      <Header />
     </div>
   );
 }
