53c6afc7b32be6937137c26853dbdba867242bc5
levblanc <levblanc@gmail.com>
2022-09-19 16:06:26 +0800

    Add enter raffle success notification UI and logic

--- components/LotteryEntrance.js
+++ components/LotteryEntrance.js
@@ -1,11 +1,14 @@
 import { useMoralis, useWeb3Contract } from 'react-moralis';
-import contractInfo from '../constants';
 import { useEffect, useState } from 'react';
 import { ethers } from 'ethers';
+import { useNotification } from '@web3uikit/core';
+import { Checkmark } from '@web3uikit/icons';
+import contractInfo from '../constants';
 
 export default function LotteryEntrance() {
   const { contractAddresses, abi } = contractInfo;
   const { chainId: chainIdHex, isWeb3Enabled } = useMoralis();
+  const dispatch = useNotification();
 
   let chainId = parseInt(chainIdHex);
 
@@ -47,8 +50,26 @@ export default function LotteryEntrance() {
     msgValue: entranceFee,
   });
 
+  const handleNewNotification = () => {
+    dispatch({
+      type: 'info',
+      message: 'Transaction Complete!',
+      title: 'Transaction Notification',
+      position: 'topR',
+      icon: <Checkmark fontSize="50px" />,
+    });
+  };
+
+  const handleEnterRaffleSuccess = async (tx) => {
+    await tx.wait(1);
+    handleNewNotification();
+  };
+
   const enterRaffleHandler = async () => {
-    await enterRaffle();
+    await enterRaffle({
+      onSuccess: handleEnterRaffleSuccess,
+      onError: (error) => console.log(error),
+    });
   };
 
   return (
